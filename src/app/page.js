94f5829062ecbf29dbd12cc7f710557c88928b28
realdeer <i3295h@naver.com>
2025-01-15 21:11:56 +0900

    타입스크립트... 적용...

--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>안녕! 나는 박물관 페이지야! 됐으면 좋겠네...</h1>
-    </div>
-  )
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React, { JSX } from 'react';
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <h1>교촌치킨 먹고 싶다.</h1>
+    </div>
+  );
+}
