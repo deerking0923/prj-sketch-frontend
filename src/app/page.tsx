fa6ab0804a168bec2628fb80aba67273881d0237
realdeer <i3295h@naver.com>
2025-02-06 00:37:12 +0900

    슬라이드 채널 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+// src/app/page.tsx 또는 원하는 경로에 Home.tsx로 저장
 import React, { JSX } from 'react';
+import Link from 'next/link';
+import './home.css';
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <h1>남주 바보보</h1>
+    <div className="container">
+      <h1 className="title">환영합니다.</h1>
+      <Link href="/slides" legacyBehavior>
+        <a className="slide-button">슬라이드 보기</a>
+      </Link>
     </div>
   );
 }
